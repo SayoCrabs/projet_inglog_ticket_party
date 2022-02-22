bc8dd1f15e8d79f786bcc5c01c6399d20fa01f63
Sandra <47884342+SayoCrabs@users.noreply.github.com>
2022-02-22 08:43:58 +0100

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutConnectionComponent } from './components/layout-connection/layout-connection.component';
+import { InputFieldComponent } from './utils/input-field/input-field.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutConnectionComponent,
+    InputFieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/layout-connection/layout-connection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-connection',
+  templateUrl: './layout-connection.component.html',
+  styleUrls: ['./layout-connection.component.css']
+})
+export class LayoutConnectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
